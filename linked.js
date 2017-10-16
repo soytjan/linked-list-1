f99274aec6cbde5a1916f3382af73fd1adbb256e
Kat <kailin.cannon@gmail.com>
2017-10-15 23:16:16 -0600

    Add function for url to be added to card

--- linked.js
+++ linked.js
@@ -3,10 +3,11 @@ var enterButton = document.getElementById("submit");
 enterButton.addEventListener('click', function() {
 	var webTitle = document.getElementById("web-title-input").value;
 	var cardWebTitle = document.getElementById("first-title");
-	var webUrl = document.getElementById("web-url-input");
-
+	var webUrl = document.getElementById("web-url-input").value;
 
 	cardWebTitle.innerText = webTitle;
+	$("[href='https://www.google.com']").attr('href', webUrl);
 	console.log("Submit button working");
-	var $cardClone = $( ".card:first" ).clone().appendTo( ".bookmark-section" ).prop({id : "display-on"});
+	$( ".card:first" ).clone().appendTo( ".bookmark-section" ).prop({id : "display-on"});
+
 });
