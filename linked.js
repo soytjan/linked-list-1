662b20478c2bee05b7063eba299ffc2f6400bb3e
Kailin Cannon <kailin.cannon@gmail.com>
2017-10-16 17:11:23 -0600

    Fix delete/read buttons

--- linked.js
+++ linked.js
@@ -1,7 +1,7 @@
 
+
+
 $(".enter-button").on('click', createBookmark)
-
-
 function createBookmark(){
 	var webTitle = $('#web-title-input').val(); 
 	var webUrl = $('#web-url-input').val();
@@ -11,10 +11,17 @@ function createBookmark(){
         <p><a href="${webUrl}">${webUrl}</a></p>
         <button class="mark-read-button" id="mark-as-read">Read</button>
         <button class="delete-button">Delete</button> 
-      </article>`
-	 );
-	console.log("Submit button working");
+      </article>`)
 }; 
 
 
-
+$(".bookmark-section").on('click', function(){
+  // event.preventDefault();
+  var currentCard = $(event.target).closest('article');
+  if(event.target.className === 'mark-read-button') {
+    $(currentCard).toggleClass('read');
+  }
+  if(event.target.className === 'delete-button') {
+    $(currentCard).remove();
+   } 
+});
