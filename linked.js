e823279a5763a1ba6ff7b0a4a1864152ecc3cb9e
Kailin Cannon <kailin.cannon@gmail.com>
2017-10-18 10:17:54 -0600

    Add reset fields

--- linked.js
+++ linked.js
@@ -1,40 +1,55 @@
 
-
-
+$('#web-title-input').keyup(function(){
+  var webTitleVal = $('#web-title-input').val();
+  if(webTitleVal === ''){
+    $('#submit').prop('disabled', true);
+  } else {
+    $('#submit').prop('disabled', false);
+  }
+});
+$('#web-url-input').keyup(function(){
+  var webUrlVal = $('#web-url-input').val();
+  if(webUrlVal === ''){
+    $('#submit').prop('disabled', true);
+  } else {
+    $('#submit').prop('disabled', false);
+  }
+});
 $('.enter-button').on('click', createBookmark)
 function createBookmark(event){
-  var webTitle = $('#web-title-input').val(); 
-  var webUrl = $('#web-url-input').val();
+  var $webTitle = $('#web-title-input'); 
+  var $webUrl = $('#web-url-input');
   var valid = validate();
   if (valid) {
-     $('.error-message').text('yo fill this out').css('display', 'block')
+     $('.error-message').text('Please enter a title and valid URL.').css('display', 'block')
   } else {
   
   $('.bookmark-section' ).append(`<article class="card" id="bookmark0">
-        <h2 id="first-title">${webTitle}</h2>
-        <p><a href="${webUrl}">${webUrl}</a></p>
+        <h2 id="first-title">${$webTitle.val()}</h2>
+        <p><a href="${$webUrl.val()}">${$webUrl.val()}</a></p>
         <button class="mark-read-button" id="mark-as-read">Read</button>
         <button class="delete-button">Delete</button> 
-        </article>`);
+        </article>`).hide().fadeIn(50);
   $('.error-message').fadeOut(800);
   countCards();
+  $webTitle[0].reset();
+  $webUrl[0].reset();
 
   };
 
 };
-
 function validate() {
-  var urlPattern = /(http|ftp|https):\/\/[\w-]+(\.[\w-]+)+([\w.,@?^=%&amp;:\/~+#-]*[\w@?^=%&amp;\/~+#-])?/
-  var webTitle = $('#web-title-input').val(); 
   var webUrl = $('#web-url-input').val();
-
-if ($.trim($('#web-url-input').val()) === urlPattern) {
-      return true;
+  var regexQuery = "^(https?://)?(www\\.)?([-a-z0-9]{1,63}\\.)*?[a-z0-9][-a-z0-9]{0,61}[a-z0-9]\\.[a-z]{2,6}(/[-\\w@\\+\\.~#\\?&/=%]*)?$";
+  var url = new RegExp(regexQuery,"i");
+    if (url.test(webUrl)) {
+        console.log('valid url: ' + webUrl);
+        return false;
     } else {
-      return false;
-    }
+    console.log('invalid url: ' + webUrl);
+    return true;
+  }
 };
-
 function countCards() {
   var read = $('.read').length;
   var totalCards = $('.card').length;
@@ -56,20 +71,4 @@ $('.bookmark-section').on('click', function(){
   }
 });
 
-// $('#web-title-input').keyup(function(){
-//   var webTitleVal = $('#web-title-input').val();
-//   if(webTitleVal === ''){
-//     $('#submit').prop('disabled', true);
-//   } else {
-//     $('#submit').prop('disabled', false);
-//   }
-// });
 
-// $('#web-url-input').keyup(function(){
-//   var webUrlVal = $('#web-url-input').val();
-//   if(webUrlVal === ''){
-//     $('#submit').prop('disabled', true);
-//   } else {
-//     $('#submit').prop('disabled', false);
-//   }
-// });
